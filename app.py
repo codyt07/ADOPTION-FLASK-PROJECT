8204c03a9a55ff6ddbd95c9f61d3d4eaf0b2da0e
codyt07 <89680528+codyt07@users.noreply.github.com>
2021-11-18 01:13:16 -0600

    second upload

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import (Flask, render_template, 
+from flask import (render_template, redirect, 
                 url_for, request)
-
-app = Flask(__name__)
-
+from models import db, Pet, app
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pets = Pet.query.all()
+    return render_template('index.html', pets=pets)
 
 @app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
-    print(request.form)
-    print(request.form['name'])
+    if request.form:
+        new_pet = Pet(name=request.form['name'], age=request.form['age'],
+        breed=request.form['breed'], color=request.form['color'], 
+        size=request.form['size'], weight=request.form['weight'],
+        url=request.form['url'], url_tag=request.form['alt'],
+        pet_type=request.form['pet'], gender=request.form['gender'],
+        spay=request.form['spay'], house_trained=request.form['housetrained'],
+        description=request.form['description'])
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('addpet.html')
+
 
 @app.route('/pet')
 def pet():
     return render_template('pet.html')
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, port=8000, host='127.0.0.1')
